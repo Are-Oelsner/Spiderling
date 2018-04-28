a51eb8ac219e134cf5d7b4c1302a209985ae2288
Are Oelsner <are.oelsner@richmond.edu>
2018-04-28 13:18:41 -0400

    Added pixelColors array from piazza to RayTracer.

--- RayTracer.h
+++ RayTracer.h
@@ -10,6 +10,9 @@ using namespace std;
 class RayTracer {
   private:
     ///Private Variables
+    unique_ptr<glm::vec4[]> pixelColors = make_unique<glm::vec4[]>(g_width*g_height); // 4 colors for alpha channel
+    // can access with
+    // glm::vec4 color = pixelColors[i*g_width + j] // access to pixelColors[i][j]
 
   public:
     /// Constructors
